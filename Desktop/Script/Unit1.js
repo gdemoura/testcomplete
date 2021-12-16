bef494503101e34a8e3742197ddb4e70b0688bf7
Gabriel De Moura <gabriel.demoura@smartbear.com>
2021-12-16 14:50:33 -0500

    Testing GitHub

---
+++ Desktop/Script/Unit1.js
@@ -0,0 +1,11 @@
+﻿function DeleteOrder()
+{
+  //Activates the 'MainForm' window.
+  Aliases.Orders.MainForm.Activate();
+  //Clicks the 'listitemGabriel' object with the right mouse button.
+  //Aliases.Orders.MainForm.OrdersView.listitemGabriel.ClickR(91, 1);
+  //Clicks the 'menuitemDeleteOrder' control.
+  Aliases.Orders.popupContext.menuitemDeleteOrder.Click();
+  //Clicks the 'buttonYes' button.
+  Aliases.Orders.dialogConfirmation.buttonYes.ClickButton();
+}
---
+++ Web/Script/ContactUsCode.js
@@ -0,0 +1,22 @@
+﻿function ContactUs()
+{
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btChrome).Run("https://smartstore.alertsite.com/");
+  Browsers.RemoteItem("http://hub.crossbrowsertesting.com:80/wd/hub", "{\"platform\":\"MacOS 11.0\",\"browserName\":\"Safari\",\"version\":\"14\",\"screenResolution\":\"1366x768\"}").Run("https://smartstore.alertsite.com/");
+  //Maximizes the specified Window object.
+  Aliases.browser.BrowserWindow.Maximize();
+  //Clicks the 'linkContactUs' link.
+  Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageContactus.Wait();
+  //Sets the text 'Gabe' in the 'textboxYourName' text editor.
+  Aliases.browser.pageContactus.sectionContent.formYourName.textboxYourName.SetText("Gabe");
+  //Sets the text 'g@smrt.com' in the 'textboxYourEmail' text editor.
+  Aliases.browser.pageContactus.sectionContent.formYourName.textboxYourEmail.SetText("g@smrt.com");
+  //Enters 'Where is my order?' in the 'textareaEnquiry' object.
+  Aliases.browser.pageContactus.sectionContent.formYourName.textareaEnquiry.Keys("Where is my order?");
+  //Clicks the 'buttonSendEmail' button.
+  Aliases.browser.pageContactus.sectionContent.formYourName.buttonSendEmail.ClickButton();
+  //Checks whether the 'contentText' property of the Aliases.browser.pageContactus.sectionContent.panelYourEnquiryHasBeen object equals 'Your enquiry has been successfully sent to the store owner.'.
+  aqObject.CheckProperty(Aliases.browser.pageContactus.sectionContent.panelYourEnquiryHasBeen, "contentText", cmpEqual, "Your enquiry has been successfully sent to the store owner.");
+}
